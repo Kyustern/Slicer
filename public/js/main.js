38c3f74ec9835f6f60c63583462797b41299652b
kyustern <leont562@gmail.com>
2019-03-20 16:54:00 +0100

    ???

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,15 @@
-const {Howl, Howler} = require('howler');
+const {Howl, Howler} = require('/public/node_modules/howler/dist/howler.js');
 
 let start = document.getElementById("start")
 let end = document.getElementById("end")
 
-let startTime = 
+let startTime = start.nodeValue
+let endTime = end.nodeValue
 
 let player = new Howl({
-    src: ['public/hehehe.mp3']
-})
+    src: ['/public/hehehe.mp3']
+});
+
+console.log("main")
 
 player.play()
