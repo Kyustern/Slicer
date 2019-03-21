eaacb956701ce07f71bf16ac68836c04449f41af
kyustern <leont562@gmail.com>
2019-03-21 17:37:51 +0100

    oooooof

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 //app.use(express.static('/public'))
-app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-})
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/public')
+// })
 
 app.listen(3000, function () {console.log("Working on port 3000")})
