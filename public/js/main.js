6a23a37d90f5d5bf6db4e653248eb259e26f0830
kyustern <leont562@gmail.com>
2019-03-21 16:53:52 +0100

    nailed stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,20 @@
-const {Howl, Howler} = require('/public/node_modules/howler/dist/howler.js');
-
 let start = document.getElementById("start")
 let end = document.getElementById("end")
 
 let startTime = start.nodeValue
 let endTime = end.nodeValue
 
-let player = new Howl({
-    src: ['/public/hehehe.mp3']
+// let player = new Howl({
+//     src: ['/public/sample.mp3']
+// });
+
+let wavesurfer = WaveSurfer.create({
+  container: "#waveform",
+  waveColor: "orange",
 });
+
+wavesurfer.load('public/sample.mp3')
 
 console.log("main")
 
-player.play()
+//player.play()
